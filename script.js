eaa358250c85033b1039462210eed015a613c99a
Liranple <Liranple@gmail.com>
2021-03-12 01:50:13 +0900

    ver 0.20.0 업데이트 진행 중

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var monsterStun = false;
-var monsterHealth;
-var createMonster = false;
-var contentArea;
-var battleText;
+var time = 0;
+var running = false;
+var timerid = 0;
+
+var charSheet;
+
+var playerAtk;
+var monsterAtk;
+var stun = false;
+var scar = false;
+
+window.onload = function () {
+    charSheet = document.getElementsByClassName("charSheet")[0]
+};
+
 
 function onlyNumber(input) {
     if (isNaN(Number(input.value))) {
@@ -20,9 +30,57 @@ function onlyNumber(input) {
         input.value = newInputValue;
     }
 }
-function screenAutoView() {
-    contentArea.scrollTop = contentArea.scrollHeight;
+
+function timerStartPause(e) {
+    if (!running) {
+        document.getElementsByClassName("turnText")[0].innerText="1 턴";
+        running = true;
+        increment();
+        e.classList.replace("fa-play", "fa-pause");
+    }
+    else if (running){
+        running = false;
+        clearTimeout(timerid);
+        e.classList.replace("fa-pause", "fa-play");
+    }
 }
+function timerReset() {
+    time = 0;
+    running = false;
+    clearTimeout(timerid);
+    document.getElementsByClassName("timer")[0].innerText="00 : 00 : 00";
+    document.getElementsByClassName("timerStartPause")[0].classList.replace("fa-pause", "fa-play");
+    document.getElementsByClassName("turnText")[0].innerText="0 턴";
+
+}
+function increment() {
+    if (running) {
+        timerid = setTimeout(function () {
+            time++;
+            var hours = Math.floor(time / 3600);
+            var mins = Math.floor(time % 3600 / 60);
+            var secs = time % 3600 % 60;
+            var turn = Math.floor((mins/15)+1);
+            if (hours < 10) {
+                hours = "0" + hours;
+            }
+            if (mins < 10) {
+                mins = "0" + mins;
+            }
+            if (secs < 10) {
+                secs = "0" + secs;
+            }
+            document.getElementsByClassName("turnText")[0].innerText=`${turn} 턴`;
+            document.getElementsByClassName("timer")[0].innerText=`${hours} : ${mins} : ${secs}`;
+            increment();
+        }, 10)
+    }
+}
+
+function addCharSheet() {
+    document.getElementsByClassName("charPanel")[0].innerHTML += charSheet.innerHTML;
+}
+
 function chanceCalc(persent) {
     var result = false
     var chance = Math.ceil(Math.random() * 100);
@@ -31,192 +89,68 @@ function chanceCalc(persent) {
     }
     return result;
 }
+function atkBoxCheck(e) {
+    var atkBox = document.getElementsByClassName(e);
+    var atkCount = 0;
+    var chanceTable = new Array(2, 7, 17, 37, 67, 107, 157, 217, 267, 307);
 
-function dicePoint(diceType) {
-    var dice = document.getElementsByName(diceType);
-    var color = ["firstDice", "secondDice", "thirdDice"];
-    var newDice = new Array(3);
-    for (var i = 0; i < dice.length; i++) {
-        if (!dice[i].checked) {
-            newDice[i] = null;
-        }
-        else {
-            newDice[i] = 0;
+    for (let i = 0; i < atkBox.length; i++) {
+        if (atkBox[i].checked) {
+            atkCount++
         }
     }
+    var damage = new Array(atkCount);
 
-    var chance = new Array(2, 7, 17, 37, 67, 107, 157, 217, 267, 307);
-
-    for (var i = 0; i < newDice.length; i++) {
-        var diceChance = Math.ceil(Math.random() * 307);
-        if (newDice[i] != null) {
-            for (var j = 0; j < chance.length; j++) {
-                if (diceChance < chance[j]) {
-                    newDice[i] = {
-                        className: color[i],
-                        value: 100 - j * 10,
-                    }
-                    break;
-                }
+    for (var i = 0; i < atkCount; i++) {
+        var atkChance = Math.ceil(Math.random() * 307);
+        for (var j = 0; j < chanceTable.length; j++) {
+            if (atkChance < chanceTable[j]) {
+                damage[i] = 100 - j * 10;
+                break;
             }
         }
     }
-    return newDice;
+    return damage;
 }
-function readyBattle() {
-    contentArea = document.getElementById("displayPanel");
-    battleText = document.createElement("p");
-
-    if (createMonster == false) {
-        if (document.getElementById("monsterHealthPoint").value > 0) {
-            monsterHealth = document.getElementById("monsterHealthPoint").value;
-            createMonster = true;
-        }
-        else {
-            battleText.innerHTML += `<span class="firstDice">마물의 체력을 설정해주세요.</span>`
-            contentArea.appendChild(battleText);
+function charSelect(e) {
+    var char = document.getElementsByName("switch");
+    for (let i = 0; i < char.length; i++) {
+        if (char[i] != e) {
+            char.checked = false;
         }
     }
 }
-function battleCheck() {
-    if (monsterHealth <= 0) {
-        battleText.innerHTML += `<span style="color: green">마물과의 전투에서 승리했습니다!</span>`;
-        createMonster = false;
-        monsterHealth = 0;
-    }
+
+function charCheck() {
+    
+
 }
-function commonAttack(diceType, bounsType) {
-    var bonus = document.getElementById(bounsType);
-    bonus = bonus.options[bonus.selectedIndex].text;
-    var dice = dicePoint(diceType);
-    var critChance = chanceCalc(30);
-    var totalDamage = 0;
-    var damageText = "";
-    var newDice = dice.filter((subject) => {
-        if (subject != null) {
-            return subject;
-        }
-    })
-    for (var i = 0; i < newDice.length; i++) {
-        totalDamage += Number(newDice[i].value);
-    }
-    var domDice = newDice.map((object) => {
-        return `<span class="${object.className}">${object.value}</span>`;
-    })
-    if (critChance) {
-        damageText += `<span class="specialWord">치명타! 1.5 X </span>`;
-        totalDamage = totalDamage * 1.5;
-    }
-    switch (diceType) {
-        case "m-dice":
-            totalDamage -= Number(bonus);
-            damageText += `(${domDice.join(" + ")}) - <span class="bounsWord">${bonus}</span>`;
-            damageText += ` = ${totalDamage} 피해를 받았다.`;
-            break;
-        case "a-dice":
-            totalDamage += Number(bonus);
-            damageText += `(${domDice.join(" + ")}) + <span class="bounsWord">${bonus}</span>`;
-            damageText += ` = ${totalDamage} 피해를 주었다.<br>`;
-            monsterHealth -= totalDamage;
-            if (monsterHealth < 0) {
-                monsterHealth = 0;
-            }
 
-            break;
-    }
+function ready() {
+    playerAtk = atkBoxCheck("pBox");
+    monsterAtk = atkBoxCheck("mBox");
 
-    return damageText;
 }
 
 function normalAttack() {
-    readyBattle();
-
-    if (createMonster) {
-        battleText.innerHTML += commonAttack("a-dice", "normal");
-        battleText.innerHTML += `마물의 남은 HP : ${monsterHealth}<br>`;
-        battleCheck();
-        if (monsterStun) {
-            monsterStun = false;
-        }
-        else if (createMonster){
-            battleText.innerHTML += commonAttack("m-dice", "armor");
-        }
-        contentArea.appendChild(battleText);
-        screenAutoView()
+    var critical = chanceCalc(30);
+    var log;
+    if (critical) {
+        log = ""
     }
 }
-function suddenAttack() {
-    readyBattle();
 
-    if (createMonster) {
-        var bonus = document.getElementById("sudden");
-        bonus = bonus.options[bonus.selectedIndex].text;
-
-        var backstep = chanceCalc(30);
-        var dice;
-        var totalDamage = 0;
-
-        if (backstep) {
-            dice = dicePoint("s-dice");
-        }
-        else {
-            dice = dicePoint("m-dice");
-        }
-        
-        var newDice = dice.filter((subject) => {
-            if (subject != null) {
-                return subject;
-            }
-        })
-        var domDice = newDice.map((object) => {
-            return `<span class="${object.className}">${object.value}</span>`;
-        })
-        for (var i = 0; i < newDice.length; i++) {
-            totalDamage += Number(newDice[i].value);
-        }
-
-        if (backstep || monsterStun) {
-            monsterStun = false;
-            totalDamage += totalDamage * 2 + Number(bonus);
-            monsterHealth -= totalDamage;
-            if (monsterHealth < 0) {
-                monsterHealth = 0;
-            }
-
-            battleText.innerHTML += `<span class="specialWord">기습 성공! 3.0 </span>X (${domDice.join(" + ")}) + <span class="bounsWord">${bonus}</span>`;
-            battleText.innerHTML += ` = ${totalDamage} 피해를 주었다.<br>`;
-            battleText.innerHTML += `마물의 남은 HP: ${monsterHealth}<br>`;
-            battleCheck();
-        }
-        else {
-            totalDamage += totalDamage * 2;
-
-            battleText.innerHTML += `<span class="specialWord">기습 실패! 3.0 </span>X (${domDice.join(" + ")})`;
-            battleText.innerHTML += ` = ${totalDamage} 피해를 받았다.<br>`;
-        }
-        contentArea.appendChild(battleText);
-        screenAutoView()
-    }
-}
-function stunAttack() {
-    readyBattle();
-
-    if (createMonster) {
-        var mDice = dicePoint("m-dice");
-        var monsterDamage = mDice[0].value * 1.1;
-
-        if (!monsterStun) {
-            monsterStun = chanceCalc(45);
-        }
-
-        if (monsterStun) {
-            battleText.innerHTML += `<span class="specialWord">무력화 성공!</span> 마물이 <span class="bounsWord">1</span>턴간 공격하지 않습니다.`;
-        }
-        else {
-            var damageText = `<span class="${mDice[0].className}">${mDice[0].value}</span>`
-            battleText.innerHTML += `<span class="specialWord">무력화 실패! 1.1</span> X ${damageText} = ${Math.ceil(monsterDamage)} 피해를 받았다.`
-        }
-        contentArea.appendChild(battleText);
-        screenAutoView()
+function playerAction(e) {
+    ready();
+    switch (e.dataset.key) {
+        case "attack":
+            
+            break;
+        case "sudden":
+            var backstep = chanceCalc(30);
+            break;
+        case "stun":
+            
+            break;
     }
 }
